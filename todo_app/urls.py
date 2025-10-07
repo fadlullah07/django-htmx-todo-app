b93c37c0b6a60f992c9ebf0ac5f541c1b541b043
fadlullah <ferfectkill@gmail.com>
2025-10-08 07:28:36 +0800

    add edit item feature

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("add-todo/", views.add_todo, name="add_todo"),
     path("update-todo/<int:pk>/", views.update_todo, name="update_todo"),
     path("delete-todo/<int:pk>/", views.delete_todo, name="delete_todo"),
+    path("edit-todo/<int:pk>/", views.edit_todo, name="edit_todo"),
+    path("get-todo-item/<int:pk>/", views.get_todo_item, name="get_todo_item"),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Todo
 
@@ -8,6 +8,11 @@ from .models import Todo
 def index(request):
     todos = Todo.objects.all().order_by("created_at")
     return render(request, "todo_app/index.html", {"todos": todos})
+
+
+def get_todo_item(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    return render(request, "todo_app/partials/_todo_item.html", {"todo": todo})
 
 
 def add_todo(request):
@@ -24,13 +29,23 @@ def update_todo(request, pk):
     todo = Todo.objects.get(pk=pk)
     todo.completed = not todo.completed
     todo.save()
-
-    # Kirim kembali hanya item yang diperbarui sebagai partial
     return render(request, "todo_app/partials/_todo_item.html", {"todo": todo})
 
 
-def delete_todo(request, pk):
+def delete_todo(_, pk):
     todo = Todo.objects.get(pk=pk)
     todo.delete()
 
     return HttpResponse(status=200)
+
+
+def edit_todo(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+
+    if request.method == "POST":
+        todo.title = request.POST.get("title", "")
+        todo.save()
+
+        return render(request, "todo_app/partials/_todo_item.html", {"todo": todo})
+
+    return render(request, "todo_app/partials/_edit_form.html", {"todo": todo})
